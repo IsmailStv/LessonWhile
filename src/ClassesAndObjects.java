0fb63b91a2341590ca8270448a630ee7f170807c
IsmailStv <erkenov@yandex.ru>
2023-01-20 21:10:56 +0300

    20/01 Type of the returned method. L15. ++

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -4,15 +4,25 @@ public class ClassesAndObjects {
         person1.name = "Roman";
         person1.age = 50;
         person1.speak();
+        int year1 = person1.calculateYearsToRetirement();
         Person person2 = new Person();
         person2.name = "Mike";
         person2.age = 20;
         person2.sayHallo();
+        int year2 = person2.calculateYearsToRetirement();
+        System.out.println("Until retirement is left " + year1 + " and " + year2);
+
+
     }
 }
 class Person{
     String name;
     int age;
+    int calculateYearsToRetirement(){
+        int year = 65 - age;
+        return year;
+//        System.out.println("Until retirement is left " + years);
+    }
     void speak(){
         for(int i = 0; i < 3; i++){
         System.out.println("My name " + name + ", " + age + " age.");
